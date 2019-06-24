03bdc8cc512ee580597d2ceb10621555a0c6ee3b
Jessie Wang <wangj.5415@gmail.com>
2019-06-24 20:16:01 +0000

    added a tail.js file which micmics the tail function on lotade

---
+++ tail.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`😡🤮🤢Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`😜😍🍆Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  let tailArray = [];
+  for (let i = 1; i < array.length ; i ++) {
+    tailArray.push(array[i]);
+  }
+  return tailArray;
+};
+
+// Test Case: Check the original array 
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+console.log(tail(words)); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
